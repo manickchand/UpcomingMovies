ad67107e727565b49ae07ac90ffea5215cc67f86
Leon Junior <lmj@icomp.ufam.edu.br>
2020-03-16 18:15:08 -0400

    (wip) upcoming list

---
+++ app/src/main/java/com/manickchand/upcomingmovies/models/movie.kt
@@ -0,0 +1,20 @@
+package com.manickchand.upcomingmovies.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+data class Movie  (
+    var id:Int?,
+    var poster_path:String?,
+    var backdrop_path:String?,
+    var title:String?,
+    var original_title:String?,
+    var popularity:Double?,
+    var overview:String?,
+    var vote_count:Int?,
+    var release_date: Date?,
+    var vote_average:Double?,
+    var genre_ids:List<Genre>?
+): Parcelable
